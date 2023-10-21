5e68e7d907c09ce8dd48a73c4b5960f483b304b5
Devin Daley <devindaley@Devins-MacBook-Air.local>
2023-10-21 17:26:22 -0500

    Camera 4

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import WebcamCapture from './components/WebcamCapture.tsx';
+import React, { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 
 function App() {
+  const webcamRef = useRef<Webcam | null>(null);
+  const [capturedImageSrc, setCapturedImageSrc] = useState<string | null>(null);
+
+  const capture = () => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setCapturedImageSrc(imageSrc);
+    }
+  };
   return (
-    <div className="App">
-      <WebcamCapture />
-    </div>
-  )
+    <div>
+    <Webcam className = "Cam"
+      audio={false}
+      ref={webcamRef}
+      screenshotFormat="image/jpeg"
+    />
+    <button className = "Capture" onClick={capture}>Capture Photo</button>
+    {capturedImageSrc && (
+    <img src={capturedImageSrc} alt="Captured"/>
+    )}
+  </div>
+  );
 }
 
 export default App;
