9ec0e4feb5ecb86d3403fed763fc48dbe973e2bd
Devin Daley <devindaley@Devins-MacBook-Air.local>
2023-10-21 16:26:11 -0500

    Camera 3

--- React/src/components/WebcamCapture.tsx
+++ React/src/components/WebcamCapture.tsx
@@ -1,22 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 function WebcamCapture() {
   const webcamRef = useRef<Webcam | null>(null);
+  const [capturedImageSrc, setCapturedImageSrc] = useState<string | null>(null);
 
   const capture = () => {
-    // Use the `getScreenshot` method to capture the picture.
     const imageSrc = webcamRef.current?.getScreenshot();
-
-    // You can now work with the captured image source, such as displaying it or sending it to the server.
     if (imageSrc) {
-      // Example: Display the captured image in an <img> element.
-      const imgElement = document.createElement('img');
-      imgElement.src = imageSrc;
-      document.body.appendChild(imgElement);
+      setCapturedImageSrc(imageSrc);
     }
   };
-
 
   return (
     <div>
@@ -26,7 +20,11 @@ function WebcamCapture() {
         screenshotFormat="image/jpeg"
       />
       <button onClick={capture}>Capture Photo</button>
-
+      {capturedImageSrc && (
+        <div>
+          <img src={capturedImageSrc} alt="Captured" />
+        </div>
+      )}
     </div>
   );
 }
